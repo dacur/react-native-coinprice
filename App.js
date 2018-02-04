f34a00f6c24cec043bba341fc32ecf75fe96e70c
David Curtis <dacur7@gmail.com>
2018-02-04 17:16:07 -0500

    Gets latest Bitcoin price in USD.

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight } from 'react-native';
 
 export default class App extends React.Component {
+  state = { price: 'Tap here for the latest Bitcoin price' }
+
+  fetchPrice() {
+    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => response.json())
+      .then(price => this.setState({ price: `$${price.bpi["USD"]["rate"]}` }));
+    this.setState({ price: '...' });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TouchableHighlight onPress={()=> this.fetchPrice()}>
+          <Text>{this.state.price}</Text>
+        </TouchableHighlight>
       </View>
     );
   }
